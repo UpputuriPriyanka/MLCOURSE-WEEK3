9ea0054073ce0b4fa51a9407a6bc26f4011aa7eb
UpputuriPriyanka <72615123+UpputuriPriyanka@users.noreply.github.com>
2020-11-08 23:37:05 +0530

    Add files via upload

---
+++ about.py
@@ -0,0 +1,10 @@
+      
+bot= {
+    'name':'Bot',
+    'tasks':[
+        {'key':'festivals','description':'Know the festivals of a month in 2020'},
+        {'key':'evaluate','description':'Evaluate/Calcualate an expression'},
+        {'key':'end','description':'Want to close the conversation'}
+    ]
+}
+
